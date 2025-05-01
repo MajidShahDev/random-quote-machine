1b1d518bf515eecc729d99eef511778c74a7fb96
MajidShahDev <mail.syedmajid@gmail.com>
2025-05-01 16:33:00 +0500

    deployment issue removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App/App.tsx'
-import Colors from './Colors.tsx'
-import Loader from './Loader.tsx'
+// import Colors from './Colors.tsx'
+// import Loader from './Loader.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
